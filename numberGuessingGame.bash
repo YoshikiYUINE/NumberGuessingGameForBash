6b6fd56fc53b6aaaa47bb2cd175cacfce4330d8e
YoshikiYUINE <yoshiki.yuine@gmail.com>
2019-08-04 09:26:47 +0900

    first commit

---
+++ numberGuessingGame.bash
@@ -0,0 +1,56 @@
+#this is bash file
+#!bin/bash/
+
+#run this file "bash ./numberGuessingGame.bash"
+
+#numberGuessingGame.bash
+echo "==============================="
+echo "Number Guessing Game"
+echo "guess random number of 1 to 100"
+echo "==============================="
+
+function numberGuessingGame(){
+	#create random number 1 to 100
+	randomNumber=$(((RANDOM%100)+1))
+	#set correct flag
+	correctFlag=false
+	#guess count 10 times
+	for i in {10..1}
+	do
+		#display remaining
+		echo "==============================="
+		echo $i "remaining"
+		echo "enter your guess number 1 to 100"
+		#get guess number from user
+		read guessNumber
+		#judge guess number integer
+		if (($guessNumber > $randomNumber))
+		then
+			echo "your enter number is $guessNumber"
+			echo -e "random number is less than $guessNumber\n"
+		elif (($guessNumber < $randomNumber))
+		then
+                        echo "your enter number is $guessNumber"
+			echo -e "random number is greater than $guessNumber\n"
+		else
+			#change correct flag from false to true
+	                ${correctFlag//false/true}
+			#out of loop
+			break
+		fi
+	done
+	#user correct
+	if $correctFlag
+	then
+		echo "correct! random number is $randomNumber"
+	#user lose
+	else
+		echo "lose! random number is $randomNumber"
+	fi
+	#close game
+	echo "finished! enter any key"
+	read anyKey
+}
+#start function numberGuessingGame
+numberGuessingGame
+#EOC
