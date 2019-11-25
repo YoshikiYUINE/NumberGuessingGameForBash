a05d51df118e4b59a32e3006a0c5d953f6f5acf0
YoshikiYUINE <yoshiki.yuine@gmail.com>
2019-11-25 22:04:05 +0900

    add check integer

--- numberGuessingGame.bash
+++ numberGuessingGame.bash
@@ -9,7 +9,7 @@ echo "Number Guessing Game"
 echo "guess random number of 1 to 100"
 echo "==============================="
 
-#diplay current date and time
+#display current date and time
 date
 echo ""
 
@@ -27,29 +27,35 @@ function numberGuessingGame(){
 		echo "enter your guess number 1 to 100"
 		#get guess number from user
 		read guessNumber
-		#judge guess number integer
-		if (($guessNumber > $randomNumber))
+		#judge guess number integer 3 digit,0 to 9
+		if [[ $guessNumber =~ [0-9]{1,3} ]]
 		then
-			echo "your enter number is $guessNumber"
-			echo -e "random number is less than $guessNumber\n"
-		elif (($guessNumber < $randomNumber))
-		then
-                        echo "your enter number is $guessNumber"
-			echo -e "random number is greater than $guessNumber\n"
+			if (($guessNumber > $randomNumber))
+			then
+				echo "your enter number is [$guessNumber]"
+				echo -e "random number is less than [$guessNumber]\n"
+			elif (($guessNumber < $randomNumber))
+			then
+				echo "your enter number is [$guessNumber]"
+				echo -e "random number is greater than [$guessNumber]\n"
+			else
+				#change correct flag from false to true
+				${correctFlag//false/true}
+				#out of loop
+				break
+			fi
 		else
-			#change correct flag from false to true
-	                ${correctFlag//false/true}
-			#out of loop
-			break
+			echo "you entered [$guessNumber]"
+			echo -e "please enter number of 1 to 100\n"
 		fi
 	done
 	#user correct
 	if $correctFlag
 	then
-		echo "correct! random number is $randomNumber"
-	#user lose
+		echo "correct! random number is [$randomNumber]"
+		#user lose
 	else
-		echo "lose! random number is $randomNumber"
+		echo "lose! random number is [$randomNumber]"
 	fi
 	#close game
 	echo "finished! enter any key"
